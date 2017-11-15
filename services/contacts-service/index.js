44e95e7e561da708414b43cbe8b195299d85814f
Armagan Amcalar <armagan@amcalar.com>
2017-11-15 16:01:28 +0300

    Add random field to list of contacts

--- services/contacts-service/index.js
+++ services/contacts-service/index.js
@@ -5,7 +5,19 @@ const res = new cote.Responder({name: 'Contact API'})
 
 res.on('*', msg => { msg.type && console.log(msg) })
 
-res.on('find all', () => Contact.find())
+res.on('find all', async () => {
+    console.log('new version')
+    let contacts = await Contact.find()
+
+    return contacts.map(contact => {
+        return {
+            id: contact.id,
+            name: contact.name,
+            address: contact.address,
+            random: Math.random()
+        }
+    });
+})
 
 res.on('add', req => Contact.create(req.contact))
 
